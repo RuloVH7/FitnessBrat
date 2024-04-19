5678bb5ae6c3dcaa860b69d51329fb5a5fbb8614
RuloVH7 <rulo_van_halen@hotmail.com>
2024-04-18 23:49:23 -0600

    Styles Logic Created

--- src/components/foodListItem.tsx
+++ src/components/foodListItem.tsx
@@ -1,23 +1,38 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const FoodListItem = ({ item }) => 
     {
       return (
-        <View style={{ 
-          backgroundColor: 'gainsboro',
-          padding: 10,
-          borderRadius: 5,
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center'}}>
-          <View style={{ flex: 1, gap: 5}}>
-            <Text style={{ fontSize: 16, fontWeight: 'bold'}}>{item.label}</Text>
-            <Text style={{ color: 'dimgray'}}>{item.cal}, {item.brand}</Text>
+        <View style={styles.container}>
+          <View style={styles.foodContainer}>
+            <Text style={styles.foodItem}>{item.label}</Text>
+            <Text style={styles.foodProperties}>{item.cal}, {item.brand}</Text>
           </View> 
           <MaterialCommunityIcons name="cookie-plus" size={24} color="royalblue" />
         </View>
       );
     };
 
-    export default FoodListItem;
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'gainsboro',
+    padding: 10,
+    borderRadius: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  foodContainer: {
+    flex: 1, gap: 5
+  },
+  foodItem: {
+    fontSize: 16, 
+    fontWeight: 'bold'
+  },
+  foodProperties: {
+    color: 'dimgray'
+  }
+})
+
+export default FoodListItem;
