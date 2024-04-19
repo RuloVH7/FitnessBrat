4661eec7131b4f9400190832bd04a562e774736f
RuloVH7 <rulo_van_halen@hotmail.com>
2024-04-19 14:07:57 -0600

    Search bar added

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TextInput, Button } from 'react-native';
 import FoodListItem from '../components/foodListItem';
+import { useState } from 'react';
 
 
 const foodItems = [
@@ -10,9 +11,22 @@ const foodItems = [
 
 export default function App() 
 {
+
+  const [search, setSearch] = useState('');
+  const performSearch = () => {
+    console.warn('Searching for: ', search)
+    setSearch('');
+  }
   return (
     <View style={styles.container}>
       {/* Food item view, or cointainer */}
+      <TextInput 
+        value={search} 
+        onChangeText={setSearch} 
+        placeholder="Search..." 
+        style={styles.input}
+      />
+      {search && <Button title='Search' onPress={performSearch}/>}
       <FlatList
         data={foodItems}
         renderItem={({ item }) => <FoodListItem item={item}/>}
@@ -28,5 +42,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     padding: 10,
+    gap: 10
   },
+  input: {
+    backgroundColor: '#f2f2f2',
+    padding: 10,
+    borderRadius: 20,
+  }
 });
